5721f212e52db3565844a47d3ab8e57cf19c41db
giadh-ai <giadh@binhduong-aptech.edu.vn>
2025-11-29 16:00:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -141,4 +141,6 @@ def lien_he():
 
 # --- KHỞI CHẠY APP ---
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+
+    #app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', debug=False)
